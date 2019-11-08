4650a8c69fb070e6392b44e7d07158af877748ef
Scott <swilson18@hark.iit.edu>
2019-11-07 19:37:23 -0600

    Create javascript.js
    added a javascript page

---
+++ js/javascript.js
@@ -0,0 +1,7 @@
+      var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 41.8615, lng: 87.6136},
+          zoom: 8
+        });
+      }
