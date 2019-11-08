2dd0c133248d03e8bb869815931b3836611f289b
Scott <swilson18@hark.iit.edu>
2019-11-07 20:36:52 -0600

    Update javascript.js
    zoom in

--- js/javascript.js
+++ js/javascript.js
@@ -2,6 +2,6 @@
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 41.8615, lng: -87.6136},
-          zoom: 10
+          zoom: 15
         });
       }
