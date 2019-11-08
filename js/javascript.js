bbd2318d57298cd41dceb9340733b6c243d8fd99
Scott <swilson18@hark.iit.edu>
2019-11-07 19:47:00 -0600

    Update javascript.js
    put in rando coordinates to see if it works

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,7 @@
       var map;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 41.8615, lng: 87.6136},
+          center: {lat: 1.8615, lng: 7.6136},
           zoom: 8
         });
       }
