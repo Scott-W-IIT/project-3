f80bc06b3fd104a3bb9ee7ef4ece0bf82785ba03
Scott <swilson18@hark.iit.edu>
2019-11-26 15:30:16 -0600

    Update javascript.js
    removed slider js

--- js/javascript.js
+++ js/javascript.js
@@ -304,17 +304,3 @@ const mapstyle = [
     ]
   }
 ];
-
-
-
-//begin slideshow
-
-var slides = document.querySelectorAll('#slides .slide');
-var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,1000);
-
-function nextSlide(){
-    slides[currentSlide].className = 'slide';
-    currentSlide = (currentSlide+1)%slides.length;
-    slides[currentSlide].className = 'slide showing';
-}
